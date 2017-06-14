45ff36ac3519e90e24a0c41a02b63530fca503cf
joaquinperaza <joaquin@peraza.uy>
2017-06-14 19:04:26 -0300

    New methods

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # SphericalUtil_Arduino
-Small aproach to Google Maps Spherical Utils library but translated to arduino, currently support only one method but will improve to support majority
+Small aproach to Google Maps Spherical Utils library but translated to arduino, currently support only some of the methods but will improve to support majority
+Current version methods:
+-Distance between two coordiantes on earth.
+-Compute positions with specific offsets from another one.
--- SphericalUtil_Arduino.h
+++ SphericalUtil_Arduino.h
@@ -1,6 +1,41 @@
+double EARTH_RADIUS = 6371009;
 
+//structure for positions of specific points
+typedef struct 
+{
+    double lt; 
+    double ln;
+}pos;
+
+double hav(double x) {
+        double sinHalf = sin(x * 0.5);
+        return sinHalf * sinHalf;
+    }
+
+double havDistance(double lat1, double lat2, double dLng) {
+        return hav(lat1 - lat2) + hav(dLng) * cos(lat1) * cos(lat2);
+    }
+
+double toRadians(double angle) {
+   return angle*0.01745329252;
+}
+double arcHav(double x) {
+        return 2 * asin(sqrt(x));
+    }
+
+double distanceRadians(double lat1, double lng1, double lat2, double lng2) {
+        return arcHav(havDistance(lat1, lat2, lng1 - lng2));
+    }
+
+double computeAngleBetween(pos from, pos to) {
+        return distanceRadians(toRadians(from.lt), toRadians(from.ln),
+                               toRadians(to.lt), toRadians(to.ln));
+    }
+double computeDistanceBetween(pos from, pos to) {
+        return computeAngleBetween(from, to) * EARTH_RADIUS;
+    }
 pos computeoffset(pos from, double distance, double heading)
-{ distance/=6371009;//earth radious
+{ distance/=EARTH_RADIUS;//earth radious
   heading*=0.01745329252;
   double fromLat = from.lt*0.01745329252;
   double fromLng = from.ln*0.01745329252;
@@ -17,13 +52,5 @@ pos computeoffset(pos from, double distance, double heading)
   offset.ln=(fromLng + dLng)/0.01745329252;
   return offset;
 }
-
-
-//structure for positions of specific points
-typedef struct 
-{
-    double lt; 
-    double ln;
-}pos;
 
 
